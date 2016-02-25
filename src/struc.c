154f8ee57adccdc127303717c666d24a3a211df8
Poussin <alexandre.sandros@gmail.com>
2016-02-25 13:07:52 +0100

    Types de données du snake
    Création des types de données pour le snake et des fonctions
    de base associées.

--- src/struc.c
+++ src/struc.c
@@ -6,3 +6,22 @@
  * @brief      Toutes les fonctions permetant d'utiliser la structure et de jouer
  * @details    --- 
  */
+
+#include "struc.h"
+/**
+ * @brief    liste de coordonnées
+ */
+struct maillon_snake
+{
+	Coord coord;/*la coordonnée dans la liste*/
+	Snake suivant;/*la suite de la liste*/
+	Snake precedent; /*la fin de la liste*/
+};
+
+struct snake
+{
+	ListeSnake tete;
+	ListeSnake dernier;
+	int longueur;
+	Direction direction;
+};
