6521df3a9e37d71efed1752229419118c2f6d531
stef <stef-x06@hotmail.fr>
2016-03-11 09:38:20 +0100

    Ajout du menu Le boutton quitter marche et erreur de segmentation pour le boutton jouer

--- src/main.c
+++ src/main.c
@@ -3,21 +3,48 @@
 
 int main(int argc, char **argv)
 {
-	
-    ClutterScript *ui;
+	ClutterScript *ui;
+    GObject *stage;
+    GError *err = NULL;
+    ClutterInitError cie;
+    cie = clutter_init(NULL, NULL);
+
+
+
     struct _snake_actor *sa;
 
-    ClutterInitError err = clutter_init(&argc, &argv);
+    // ClutterContent *image = generate_image("data/menu.jpg");
+
 
     ui = clutter_script_new();
-    clutter_script_load_from_file(ui, "src/gui/stage.json", NULL);
+    clutter_script_load_from_file(ui, "src/gui/menu.json", &err);
+    
+    if (err != NULL) {
+        printf("%s\n", err->message);
+        err=NULL;
+    }
+
+    stage = clutter_script_get_object(ui, "stage");
+
+    /* Relie les signaux définis dans le fichier JSON */
     clutter_script_connect_signals(ui, ui);
 
+    /* connecter à la main :
+    g_signal_connect (bouton,
+                   "Clicked",
+                   G_CALLBACK (_button_clicked_cb),
+                   actor_to_move);  */
 
+    clutter_actor_show(CLUTTER_ACTOR(stage));
 
-    init_view(ui, 30, 30, DROITE, 20, coord_from_xy(22, 2));
+    clutter_main();
+
 
     return 0;
 	 
 }
-	
+ /*
+    ui = clutter_script_new();
+    clutter_script_load_from_file(ui, "src/gui/stage.json", NULL);
+    clutter_script_load_from_file(ui, "src/gui/menu.json", NULL);
+*/
---
+++ src/menu.c
@@ -0,0 +1,48 @@
+#include <clutter/clutter.h>
+#include <stdlib.h>
+#include "affichage.h"
+
+gboolean jouer_clicked_cb(ClutterClickAction *action,
+                            ClutterActor *actor,
+                            gpointer data)
+{
+	//clutter_actor_hide(actor);
+	printf("Jouer\n");
+
+    ClutterScript *ui;
+    GError *err = NULL;
+    ui = clutter_script_new();
+    clutter_script_load_from_file(ui, "src/gui/stage.json", &err);
+    
+    if (err != NULL) {
+        printf("%s\n", err->message);
+        err=NULL;
+    }
+
+    init_view(ui, 30, 30, DROITE, 20, coord_from_xy(22, 2));
+    
+	return TRUE;
+}
+
+gboolean quitter_clicked_cb(ClutterClickAction *action,
+                            ClutterActor *actor,
+                            gpointer data)
+{
+
+	printf("Quitter\n");
+    clutter_main_quit();
+	return TRUE;
+}
+
+gboolean button_enter_cb(ClutterActor *actor, ClutterEvent *event, gpointer user_data) {
+    ClutterColor color = { 150, 234, 254, 255 };
+
+    clutter_actor_set_background_color(actor, &color);
+    return TRUE;
+}
+
+gboolean button_leave_cb(ClutterActor *actor, ClutterEvent *event, gpointer user_data) {
+
+    clutter_actor_set_background_color(actor, CLUTTER_COLOR_White);
+    return TRUE;
+}
